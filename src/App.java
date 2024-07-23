d89396aae6ab807764bc27c1dc4e2938ea0da8c4
Gabriel <gabrieltroop27@gmail.com>
2024-07-22 21:00:23 -0300

    Aula2 DIO

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+public static void main (String [] args) 
+    {   
+        // Aula 1
+        System.out.print("Hello World!" );
+        
+        // Aula 2
+        @SuppressWarnings("unused")
+        final String BR = "Brasil"; // final == const
+        
+        @SuppressWarnings("unused")
+        boolean verdadeira = true;
+
+
     }
 }
