cd07d8c00d0dcf2e51c8049e3c75acd2729470f2
Gabriel <gabrieltroop27@gmail.com>
2025-03-02 02:22:50 -0300

    To finish beans versus components class in DIO 3:30

---
+++ DecoleTech2025/first-steps/src/main/java/dio/app/ConversorJson.java
@@ -0,0 +1,13 @@
+package dio.app;
+
+import com.google.gson.Gson;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConversorJson {
+    private Gson gson = new Gson();
+    public ViaCepResponse converter(String json){
+        ViaCepResponse response = gson.fromJson(json, ViaCepResponse.class);
+        return response;
+    }
+}
