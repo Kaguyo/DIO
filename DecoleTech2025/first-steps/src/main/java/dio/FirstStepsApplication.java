b6a9e10d3a6455c600de20e6898308d5a6470708
Gabriel <gabrieltroop27@gmail.com>
2025-03-04 22:32:01 -0300

    fiknished prototype vs singleton, autowired components and beans

--- DecoleTech2025/first-steps/src/main/java/dio/FirstStepsApplication.java
+++ DecoleTech2025/first-steps/src/main/java/dio/FirstStepsApplication.java
@@ -1,13 +1,27 @@
 package dio;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import dio.app.ConversorJson;
+import dio.app.ViaCepResponse;
 
 @SpringBootApplication
 public class FirstStepsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstStepsApplication.class, args);
+		
 	}
+	 @Bean
+	public CommandLineRunner run(ConversorJson conversor) throws Exception {
 
+		return args -> {
+			String json = "{\"cep\": \"01001-000\", \"logradouro\": \"Praça da Sé \", \"localidade\" : \" São Paulo\"}";
+			ViaCepResponse response = conversor.converter(json);
+			System.out.println("Dados do CEP: " + response);
+		};
+	}
 }
