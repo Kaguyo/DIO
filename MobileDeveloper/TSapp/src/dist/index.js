1b868e690c5dfa9f78c3cc33d0e635bff29597fd
= <=>
2025-09-01 14:34:27 -0300

    feat: even more knowledge in node

---
+++ MobileDeveloper/TSapp/src/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function main() {
+    const greeting = "Hello, TypeScript!";
+    console.log(greeting);
+}
+main();
+// npx tsc --init
---
+++ MobileDeveloper/TSapp/src/index.ts
@@ -0,0 +1,9 @@
+
+function main() {
+    const greeting: string = "Hello, TypeScript!";
+    console.log(greeting);  
+}
+
+main();
+
+// npx tsc --init
