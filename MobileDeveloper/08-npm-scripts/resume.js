f73b7a5a37c95718187176b9302f7efd0e3c94b6
= <=>
2025-09-01 13:34:56 -0300

    feature: more knowledge in node :)

---
+++ MobileDeveloper/08-npm-scripts/resume.js
@@ -0,0 +1,5 @@
+// executed npm init -y
+// type module in package.json
+// create index.js that does something
+// define a script in the scripts object in package.json
+// use node run <script-name> to execute index.js
